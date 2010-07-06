d7f545b4a692b6f864f283815cca63350ce87145
Jason Smith <jhs@couch.io>
2010-07-06 23:05:49 +0700

    Just logging and trying to parse the incoming XML

--- shows/jason.js
+++ shows/jason.js
@@ -6,6 +6,18 @@ function(doc, req) {
 
   if(req.method == 'OPTIONS')
     return dav.response({
-      headers: {"Allow": "GET, HEAD, COPY, MOVE, POST, PUT, PROPFIND, PROPPATCH, OPTIONS, MKCOL, DELETE, TRACE, REPORT, LOCK, UNLOCK"}
+      headers: {"Allow": "GET, HEAD, COPY, MOVE, POST, PUT, PROPFIND, PROPPATCH, OPTIONS, MKCOL, DELETE"
+                         //+ "REPORT, TRACE, LOCK, UNLOCK"
+               }
     })
+
+  util.dir({doc:doc, req:req});
+
+  if(req.method == 'PROPFIND') {
+    log("body = " + req.body);
+    var r = new XML(req.body);
+    log("r = " + r);
+    util.dir({request:request});
+    return "I got it";
+  }
 };
